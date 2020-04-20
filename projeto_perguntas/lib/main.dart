b0cceaaab4160466e38ee965aa656a7cf1831ea2
angeloconfer <angeloconfer@hotmail.com>
2020-04-19 22:24:32 -0300

    projeto perguntas

---
+++ projeto_perguntas/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(PerguntaApp());
+
+class PerguntaApp extends StatelessWidget {
+  void responder() {
+    print('Pergunta respondida');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita ?',
+      'Qual é o seu animal favorito ?',
+    ];
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Perguntas'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text(perguntas.elementAt(0)),
+              RaisedButton(child: Text('Resposta 1'), onPressed: responder),
+              RaisedButton(child: Text('Resposta 2'), onPressed: responder),
+              RaisedButton(child: Text('Resposta 3'), onPressed: responder)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
