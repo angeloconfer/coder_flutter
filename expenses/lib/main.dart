539e7afed74defb0fad483caf306a3497683f681
angeloconfer <angeloconfer@hotmail.com>
2020-05-05 13:23:58 -0300

    ExpensesApp - planilha pessoal

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -64,7 +65,7 @@ class MyHomePage extends StatelessWidget {
                       ),
                       padding: EdgeInsets.all(10),
                       child: Text(
-                        tr.value.toString(),
+                        'R\$ ${tr.value.toStringAsFixed(2)}',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
@@ -80,9 +81,9 @@ class MyHomePage extends StatelessWidget {
                               fontWeight: FontWeight.bold,
                             )),
                         Text(
-                          tr.date.toString(),
+                          DateFormat('d MMM y').format(tr.date),
                           style: TextStyle(
-                            color:Colors.grey,
+                            color: Colors.grey,
                           ),
                         ),
                       ],
